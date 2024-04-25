4c68a03966cbbaed8085ae993e122a4d215998ab
Suguru Wada <pxh02520@nifty.com>
2024-04-25 09:50:29 +0900

    add php-qrcode

--- project-template/laravel/vendor/composer/installed.php
+++ project-template/laravel/vendor/composer/installed.php
@@ -28,6 +28,24 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
+        'chillerlan/php-qrcode' => array(
+            'pretty_version' => '5.0.2',
+            'version' => '5.0.2.0',
+            'reference' => 'da5bdb82c8755f54de112b271b402aaa8df53269',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chillerlan/php-qrcode',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'chillerlan/php-settings-container' => array(
+            'pretty_version' => '2.1.5',
+            'version' => '2.1.5.0',
+            'reference' => 'f705310389264c3578fdd9ffb15aa2cd6d91772e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chillerlan/php-settings-container',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
         'cordoval/hamcrest-php' => array(
             'dev_requirement' => true,
             'replaced' => array(
